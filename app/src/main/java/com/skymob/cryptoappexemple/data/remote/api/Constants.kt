7c7165e26c279085b614b5419ecc7e7b3440ddd1
caio_ <caiooalexandre94@gmail.com>
2025-01-05 15:19:29 -0300

    chore: Adiciona arquivo Constants faltante

---
+++ app/src/main/java/com/skymob/cryptoappexemple/data/remote/api/Constants.kt
@@ -0,0 +1,11 @@
+package com.skymob.cryptoappexemple.data.remote.api
+
+import com.skymob.cryptoappexemple.BuildConfig
+
+
+object Constants {
+
+    const val BASE_URL = "https://pro-api.coinmarketcap.com/v1/"
+    const val APIKEY =  BuildConfig.API_KEY
+
+}
